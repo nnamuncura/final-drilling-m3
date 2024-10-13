be4449a589d4f1749cfffca45c8d0ef57cdba789
nnamuncura <nnamuncura@icloud.com>
2024-10-13 01:48:25 -0300

    (WIP): Se agrega funcionalidad a boton del carrito - y +, tambien se agrega descuento en compras sobre los $30.000

--- assets/js/main.mjs
+++ assets/js/main.mjs
@@ -1,6 +1,5 @@
 import Producto from "./class/producto.mjs";
 import Carrito from "./class/Carrito.mjs";
-console.log('main.mjs funcionando...')
 
 let iconCart = document.querySelector('.icon-cart');
 let closeCart = document.querySelector('.close');
@@ -8,11 +7,10 @@ let body = document.querySelector('body');
 let listProductHTML = document.querySelector('#listProduct');
 let listCartHTML = document.querySelector('.listCart');
 let totalCartSpan = document.querySelector('.total span');
+let discountCartSpan = document.querySelector('.discount span');
 let iconCartSpan = document.querySelector('.icon-cart span');
 
 let url = './assets/js/products-copy.json';
-let listProducts = [];
-let carts = [];
 let carritoDeCompra = []
 
 //muestra el carrito en pantalla haciendo clic en el icono 🛒
@@ -25,144 +23,79 @@ closeCart.addEventListener('click', () => {
     body.classList.toggle('showCart')
 })
 
-//funcion que pinta las card con datos en la pantalla
-// const addDataToHTML = () => {
-//     listProductHTML.innerHTML = '';
-//     if (listProducts.length > 0) {
-//         listProducts.forEach(product => {
-//             let newProduct = document.createElement('div');
-//             newProduct.classList.add('col-12', 'col-sm-4', 'col-md-3');
-//             newProduct.innerHTML = `
-//                 <div class="card my-3">
-//                     <img src="${product.image}" class="card-img-top" alt="">
-//                     <div class="card-body" data-id="${product.id}">
-//                         <h6 class="card-title">${product.name}</h6>
-//                         <p class="card-text"><strong>$ ${product.price}</strong></p>
-//                         <button class="btn btn-success addCart">Agregar al Carro</button>
-//                     </div>
-//                 </div>
-//             `;
-//             listProductHTML.appendChild(newProduct);
-//         })
-//     }
-// }
-
-// listProductHTML.addEventListener('click', (event) => {
-//     let positionClick = event.target;
-//     if (positionClick.classList.contains('addCart')) {
-//         let product_id = positionClick.parentElement.dataset.id;
-//         console.log('id del producto en listProductHTML.addEventListner() L53: ', product_id);
-//         addProductToCart(product_id)
-//     }
-// })
-
-// const addProductToCart = (product_id) => {
-//     console.log('dentro de funcion addPruductTocart()')
-//     let positionThisProductInCart = carts.findIndex((value) => value.product_id == product_id);
-//     if (carts.length <= 0) {
-//         carts = [{
-//             product_id: product_id,
-//             quantity: 1
-//         }]
-//     } else if (positionThisProductInCart < 0) {
-//         carts.push({
-//             product_id: product_id,
-//             quantity: 1
-//         });
-//     } else {
-//         carts[positionThisProductInCart].quantity = carts[positionThisProductInCart].quantity + 1;
-//     }
-//     console.log(carts);
-// }
-
-
-//funcion que conecta a la base de datos con fetch y llama a funcion pintar card en pantalla
+//Conecta a la base de datos con fetch y llama a funcion pintar card en pantalla
 const showProductOnScreen = () => {
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            //listProducts = data;
-            //console.log('print desde showProduct()', listProducts);
             const productos = data.productos;
-            console.log('productos: ', productos)
             return productos.map(producto => {
                 return new Producto(producto)
             })
-            //se llama funcion que pinta las card en pantalla
-            //addDataToHTML();
         })
         .then(productos => {
             listProductHTML.innerHTML = '';
-            console.log('productos: ', productos)
-            let mensajePantalla = ''
-            productos.forEach((producto, indice) => {
-                let newProduct = document.createElement('div');
-                newProduct.classList.add('col-12', 'col-sm-4', 'col-md-3');
-                const { id, name, price } = producto;
-                newProduct.innerHTML = `
-                <div class="card my-3">
-                    <img src="${producto.image}" class="card-img-top" alt="">
-                    <div class="card-body" data-id="${id}">
-                        <h6 class="card-title">${name}</h6>
-                        <p class="card-text"><strong>$ ${price}</strong></p>
-                        <button class="btn btn-success addCart">Agregar al Carro</button>
-                    </div>
-                </div>
-                `;
-                listProductHTML.appendChild(newProduct)
-                //agregar productos al carrito
-                newProduct.getElementsByTagName('button')[0].addEventListener('click', (event) => agregarAlCarrito(event, producto))
-            })
-            //console.log('mensajePantalla : ',mensajePantalla)
-            //let eleccionUsuario = prompt(mensajePantalla)
-            //let carrito = [];
-            //agregarAlCarrito(productos)
-
+            //console.log('productos: ', productos)
+            productos.forEach((producto, indice) => pintarProductosEnPantalla(producto))
         })
 }
 
+//Agrega los productos al carrito
 function agregarAlCarrito(event, producto) {
-    console.log('Productos desde agregarAlCarrito(): ')
     let positionClick = event.target;
     let product_id = positionClick.parentElement.dataset.id;
-    console.log('positionClick : ', positionClick)
-    console.log('Id producto click : ', product_id)
     if (!carritoDeCompra) {
         carritoDeCompra = new Carrito(producto)
     } else {
         const indiceProducto = carritoDeCompra.findIndex(producto => producto.id == product_id)
-        console.log('indiceProducto L124 : ', indiceProducto)
         if (indiceProducto === -1) {
             producto.quantity = 1;
             carritoDeCompra.push(producto)
         } else {
             let positionItemInCart = carritoDeCompra.findIndex(i => i.id == product_id)
-            console.log('carritodecompra[product_id] : ', carritoDeCompra[positionItemInCart].id, carritoDeCompra[positionItemInCart].name)
+            //console.log('carritodecompra[product_id] : ', carritoDeCompra[positionItemInCart].id, carritoDeCompra[positionItemInCart].name)
             carritoDeCompra[positionItemInCart].quantity++
         }
     }
-    console.log('En carrito hay: ', carritoDeCompra);
+    //console.log('En carrito hay: ', carritoDeCompra);
     // pintar carrito en html
     pintarProductosEnCarrito(carritoDeCompra)
 }
 
+//Agrega las card con productos en pantalla
+function pintarProductosEnPantalla(producto){
+    let newProduct = document.createElement('div');
+    newProduct.classList.add('col-12', 'col-sm-4', 'col-md-3');
+    const { id, name, price } = producto;
+    newProduct.innerHTML = `
+    <div class="card my-3">
+        <img src="${producto.image}" class="card-img-top" alt="">
+        <div class="card-body" data-id="${id}">
+            <h6 class="card-title">${name}</h6>
+            <p class="card-text"><strong>$ ${price}</strong></p>
+            <button class="btn btn-success addCart">Agregar al Carro</button>
+        </div>
+    </div>
+    `;
+    listProductHTML.appendChild(newProduct)
+    //agregar productos al carrito
+    newProduct.getElementsByTagName('button')[0].addEventListener('click', (event) => agregarAlCarrito(event, producto))
+}
+
+//Muestra los productos del carrito en pantalla
 function pintarProductosEnCarrito(carritoDeCompra){
     listCartHTML.innerHTML = '';
     let totalQuantity = 0;
     let totalToPay = 0;
+    let discount = 0;
     if (carritoDeCompra.length > 0) {
-        console.log('estoy revisando el carrito : ', carritoDeCompra.length)
-        console.log('Elementos en el carrito para pintar el html', carritoDeCompra)
+        //console.log('Elementos en el carrito para pintar el html', carritoDeCompra)
         carritoDeCompra.forEach(cart => {
             const {image, id, name, price, quantity } = cart;
             totalQuantity = totalQuantity + cart.quantity;
             let newCart = document.createElement('div');
             newCart.classList.add('item');
-            //newCart.dataset.id = cart.product_id;
-            //console.log('newCartDataseID-L79', newCart.dataset.id)
-            //let positionProduct = listProducts.findIndex((value) => value.id == cart.product_id);
-            //let info = listProducts[positionProduct];
-            //totalToPay = totalToPay +(info.price * cart.quantity);
+            newCart.dataset.id = id;
             totalToPay = totalToPay + (price * quantity);
             newCart.innerHTML = `
                     <div class="image">
@@ -183,10 +116,46 @@ function pintarProductosEnCarrito(carritoDeCompra){
             listCartHTML.appendChild(newCart);
         })
     }
+    totalToPay >=30000? discount = totalToPay*0.1 : discount = 0;
     iconCartSpan.innerHTML = totalQuantity;
-    totalCartSpan.innerHTML = totalToPay;
+    discountCartSpan.innerHTML = discount;
+    totalCartSpan.innerHTML = totalToPay-discount;
 }
 
+//Captura el evento click en boton - o + del carrito
+listCartHTML.addEventListener('click', (event) => {
+    let positionClick = event.target;
+    if (positionClick.classList.contains('minus') || positionClick.classList.contains('plus')) {
+        let product_id = (positionClick.parentElement).parentElement.dataset.id;
+        let type = 'minus';
+        if(positionClick.classList.contains('plus')){
+            type = 'plus';
+        }
+        changueQuantity(product_id, type);
+    }
+})
+
+//Modifica cantidades del carrito 
+const changueQuantity = (product_id, type)=>{
+    let positionItemInCart = carritoDeCompra.findIndex((value) => value.id == product_id);
+    if(positionItemInCart >=0){
+        switch(type){
+            case 'plus':
+                carritoDeCompra[positionItemInCart].quantity = carritoDeCompra[positionItemInCart].quantity +1;
+                //console.log('posicion en carro en plus es: ',carritoDeCompra[positionItemInCart].quantity)
+                break;
+            default:
+                let valueChange = carritoDeCompra[positionItemInCart].quantity -1;
+                if(valueChange > 0){
+                    carritoDeCompra[positionItemInCart].quantity = valueChange;
+                }else{
+                    carritoDeCompra.splice(positionItemInCart, 1);
+                }
+                break;
+        }
+    }
+    pintarProductosEnCarrito(carritoDeCompra)
+}
 
 //Se llama a funcion que muestra los productos en pantalla
 showProductOnScreen();
